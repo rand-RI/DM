0ba66c6f0e7d5f100c2a99eefdc7e5110764504c
DanielMurray3 <daniel.murray3@uq.net.au>
2014-10-16 14:58:03 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 #stage1: IMPORT LIBRARY
-import pandas.io.data as pdio
+import pandas.io.data as pdio      #import pandas.io.data library
+import SystemicRiskMeasures as srm #import Systemic Risk Measures library
 import matplotlib.pyplot as plt
-from mahalanobis_distance import MahalanobisDist   #import Kritzman and Li - 2010 - Skulls, Financial Turbulence, and Risk Management
-from correlation_surprise import Correlation_Surprise  # import Kinlaw and Turkington - 2012 - Correlation Surprise
-
-
 #stage2: IMPORT DATA WITH ANY NUMBER OF PORTFOLIOS
 
-symbols = ['FTSEMIB.MI','^IXIC','RTS.RS','^AORD','^BSESN','^BVSP','^FCHI','^FTSE','^GDAXI','^GSPTSE','^JKSE','^KS11','^MERV','^MXX','^N225','^SSEC'] # List all stock symbols to download in alphabetical order
-#G20 MEMBERS
+          #Must arrange tickers in alphabetical order
+symbols = ['FTSEMIB.MI','RTS.RS','^AORD','^BSESN','^BVSP','^FCHI','^FTSE','^GDAXI','^GSPTSE','^IXIC','^JKSE','^KS11','^MERV','^MXX','^N225','^SSEC'] # List all stock symbols to download in alphabetical order
+#G20 MEMBERS#
 #Argentina:^MERV is considered the most important index of Argentina's primary stock exchange "Buenos Aires Stock Exchange"
 #Austrlaia: ^AORD is considered the oldest index of shares in Australia
 #Brazil: ^BVSP is an index of 50 stocks that are traded on the Sao Paulo Stock Exchange in Brazil
@@ -30,32 +28,46 @@ symbols = ['FTSEMIB.MI','^IXIC','RTS.RS','^AORD','^BSESN','^BVSP','^FCHI','^FTSE
 #United States of America:'^IXIC'
 #the European Union: need to collect all countries?
 
-stock_data = pdio.get_data_yahoo(symbols,start='1/1/2000',end='1/10/2014') # Download data from YAHOO as a pandas Panel object
+#stage3: DOWNLOAD DATA AND CALCULATE RETURN VALUES
 
-# Convert data frame prices into returns data
-adj_close = stock_data['Adj Close'] # Scrape adjusted closing prices as pandas DataFrane object
-returns = np.log(adj_close/adj_close.shift(1)) # Continuously compounded returns
-
-# Calculate systemic risk measures
-SRM_mahalanobis = srm.mahalanobis_distance(returns)
-SRM_correlationsurprise = srm.correlationsurprise(returns)
-SRM_absorptionratio = srm.absorptionratio(returns)
-
-systemicRiskMeasure = [SRM_mahalanobis SRM_correlationsurprise SRM_absorptionratio]   # group systemic risk measures
-
-for sysRiskMeasure in systemicRiskMeasure
-	fig = print_systemic_Risk(systemicRiskMeasure[sysRiskMeasure])
-	fig.savefig("{}.jpg".format(sysRiskMeasure))
-
-# Plotting the systemic risk measures
-
-def print_systemicRiskMeasure:
-	SRM_mahalanobis.plot()               #Plot MD
-	plt.ylabel('Index')
-	plt.xlabel('Year')
+Historical_Prices = pdio.get_data_yahoo(symbols,start='1/1/2007',end='1/10/2009') # Download data from YAHOO as a pandas Panel object
+Adjusted_Close_Prices = Historical_Prices['Adj Close'].dropna()  # Scrape adjusted closing prices as pandas DataFrane object while also removing all Nan data
+returns = np.log(Adjusted_Close_Prices/Adjusted_Close_Prices.shift(1)).dropna()  # Continuously compounded returns while also removing top row of Nan data
 
 
+#stage4: Import Systemic Risk Measures
+SRM_mahalanobis= srm.MahalanobisDist(returns)       #define Mahalanobis Distance Formula
+SRM_correlationsurprise= srm.Correlation_Surprise(returns)#define Correlation Surprise Score
+SRM_absorptionratio= srm.Absorption_Ratio(returns)#define Absorption Ratio
+
+systemicRiskMeasure= [SRM_mahalanobis,SRM_correlationsurprise,SRM_absorptionratio]
+
+#for sysRiskMeasure in systemicRiskMeasure:
+   # fig= print_systemic_Risk(systemicRiskMeasure[sysRiskMeasure])
+   # fig.savefig("{}.jpg".format(sysRiskMeasure))
+    
+    
+#Plot SRM_mahalanobis
+plt.xticks(rotation=50)
+plt.xlabel('Year')
+plt.ylabel('Index')
+plt.bar(SRM_mahalanobis.index,SRM_mahalanobis.values, width=2)
+
+#Plot Correlation_surprise
+Correlation_Surprise= SRM_correlationsurprise[0]
+Turbulence_Score= SRM_correlationsurprise[1]
+plt.xlabel('Turbulence Score')
+plt.ylabel('CorrelationSurprise')
+plt.ylim([0,1.1])
+plt.xlim([0,1.1])
+plt.scatter(Turbulence_Score,Correlation_Surprise)      #Plot SRM_correlationsurprise
 
 
 
+
+#systemicRiskMeasure = [SRM_mahalanobis, SRM_correlationsurprise] # group systemic risk measures
+
+#what I need to do is finish AB
+#plot graphs using the SRM command from the github references
+#remove TB from Correlation Surprise and just use MD
 
