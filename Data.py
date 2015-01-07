4aacc49f6a38fd86c522549e5111aca2468ed670
DanielMurray3 <daniel.murray3@uq.net.au>
2015-01-07 22:43:54 +1000

    update

--- Data.py
+++ Data.py
@@ -1,41 +1,43 @@
-import pandas.io.data as pdio      #import pandas.io.data library
-import numpy as np
-import pandas as pd
 
-        #MahalanoBis Distance
-symbols = ['^GSPC','EFA','^TYX','TIP','VNQ','^DJC'] 
-#['^GSPC','^NYA','^IXIC','^GSPC','^TYX','^DJC']
-#['^AORD','^ATX','^BFX','^BSESN','^BVSP','^FCHI','^GDAXI','^GSPC','^GSPTSE','^HSI','^JKSE','^KLSE','^KS11','^MERV','^MXX','^N225','^SSEC','^STI','^TWII'] # List all stock symbols to download in alphabetical order
-Start_Date='10/20/1973'#MM,DD,YY
-End_Date='12/8/2014'#MM,DD,YY
-frequency='d'
+""" This Python scripts downloads all the required data for each Systemic Risk Paper
+    1. Malanhobis Distance
+    2. Correlation Surprise
+    3. Absorption Ratio
+    ....                                                                            """
 
-Historical_Prices = pdio.get_data_yahoo(symbols,start= Start_Date,end= End_Date) 
-Historical_Prices.save('returns')
+                #STAGE1: Import Python libraries 
+
+import pandas.io.data as pdio                                                  #Functions from pandas.io.data extract data from various Internet sources into a DataFrame. Currently the following sources are supported: Yahoo Finance, Google Finance, FRED, Kenneth French's library, World Bank and Google Analytics  
+import numpy as np                                                             #NumPy is an extension to the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large library of high-level mathematical functions to operate on these arrays
+import pandas as pd                                                            #Pandas is a software library written for the Python programming language for data manipulation and analysis.
+
+                #STAGE2: Download Required Data
+
+        # MahalanoBis Distance
+#Figure 4
+symbols = ['^GSPC','EFA','^TYX','TIP','VNQ','^DJC']                            #An example of the type of data that would be required for Mahalanobis Distance inputs
+Start_Date,End_Date='10/20/1973','12/8/2014'                                   #Date Range for required returns. In the format of MM/DD/YY
+Historical_Prices = pdio.get_data_yahoo(symbols,start= Start_Date,end= End_Date)# Download Historical Prices using symbols as a list of yahoo tickers over the defined start date-end date
+Historical_Prices.save('returns')                                              #Save Figure 4's data as 'returns' 
 
 #Figure 5 MD
-symbols = ['XWD.TO','^RUT','^GSPC','IWF','IWD','HDG'] 
-Start_Date='1/1/1993'#MM,DD,YY
-End_Date='12/8/2014'#MM,DD,YY
-frequency='d'
-
-Historical_Pricess = pdio.get_data_yahoo(symbols,start= Start_Date,end= End_Date) 
-Historical_Pricess_closed=Historical_Pricess['Adj Close'].dropna()
-Figure5= pd.DataFrame(index=Historical_Pricess_closed.index)
-Figure5['World Equties']=Historical_Pricess_closed['HDG'].values
-Figure5['Small-Large']=Historical_Pricess_closed['^RUT'].values-Historical_Pricess_closed['^GSPC'].values
-Figure5['Growth-Value']=Historical_Pricess_closed['IWF'].values-Historical_Pricess_closed['IWD'].values
-Figure5['Hedge Funds']=Historical_Pricess_closed['HDG'].values
-Figure5.save('returnsMD_Figure5')
+symbols = ['XWD.TO','^RUT','^GSPC','IWF','IWD','HDG']                          #An example of the type of data that would be required for Figure 5 inputs 
+Start_Date,End_Date='1/1/1993','12/8/2014'                                     #Date Range for required returns. In the format of MM/DD/YY 
+Historical_Pricess = pdio.get_data_yahoo(symbols,start= Start_Date,end= End_Date)#Download Historical Prices using symbols as a list of yahoo tickers over the defined start date-end date
+Historical_Pricess_closed=Historical_Pricess['Adj Close'].dropna()             # From Historical Prices extract the Adjusted CLose Returns 
+Figure5= pd.DataFrame(index=Historical_Pricess_closed.index)                   #Create Figure 5 DataFrame with Historical_Prices dates as index.             
+Figure5['World Equties']=Historical_Pricess_closed['HDG'].values               #Append World Equties to DataFrame 
+Figure5['Small-Large']=Historical_Pricess_closed['^RUT'].values-Historical_Pricess_closed['^GSPC'].values#Append Small-Large to DataFrame
+Figure5['Growth-Value']=Historical_Pricess_closed['IWF'].values-Historical_Pricess_closed['IWD'].values#Append Growth-Value to DataFrame
+Figure5['Hedge Funds']=Historical_Pricess_closed['HDG'].values                 #Append Hedge Funds to DataFrame
+Figure5.save('returnsMD_Figure5')                                              #save Figure 5's DataFrame as returns Mahalanobis Distance Figure 5
 
 
 #Table 1
-symbols = ['GMWAX','AAT','IYF']                                                     #Import Table 1 Data for Malanobis Distance Paper
-Start_Date='1/1/1993'#MM,DD,YY
-End_Date='12/8/2014'#MM,DD,YY
-frequency='d'
-Historical_Pricess = pdio.get_data_yahoo(symbols,start= Start_Date,end= End_Date)
-Historical_Pricess.save('Table_1')
+symbols = ['GMWAX','AAT','IYF']                                                #Import Table 1 Data for Malanobis Distance Paper
+Start_Date,End_Date='1/1/1993','12/8/2014'                                     #MM,DD,YY
+Historical_Pricess = pdio.get_data_yahoo(symbols,start= Start_Date,end= End_Date)#Download Historical Prices using symbols as a list of yahoo tickers over the defined start date-end date
+Historical_Pricess.save('Table_1')                                             #save Table 1 returns as Table_1 
 
 
         #Correlation Surprise
--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-                             #1: IMPORT LIBRARY
-import pandas.io.data as pdio      
-import systemicRiskMeasures as srm                                                 #import Systemic Risk Measures library
-import matplotlib.pyplot as plt
+ 
+""" This Python scripts is used to generate systemic risk returns from systemic risk modules
+    imported from systemicRiskMeasures as srm  in or each Systemic Risk Paper
+                                                                                """
+
+
+                #STAGE 1: IMPORT LIBRARY
+import systemicRiskMeasures as srm                                              #import Systemic Risk Measures library
 import pandas as pd
-import datetime as dt 
-import numpy as np
 
-                            #2: IMPORT DATA
-
+                #STAGE 2: IMPORT DATA
     #Mahalanobis Distance
-#Data
-Historical_Prices= pd.load('returns')                                              #Import Hisorical Prices for six asset-class indices: US stocks, non-US Stocks, US Bonds, non-US bonds, commodities, and US real estate
-Adjusted_Close_Prices= Historical_Prices['Adj Close'].dropna()                     #Extract Adjusted Close prices from Historial Prices of the six assets 
-returns= srm.logreturns(Adjusted_Close_Prices)                                     #Convert Adjusted Close Prices from daily returns to Logarithmic returns
+Historical_Prices= pd.load('returns')                                          #Import Historical Prices for six asset-class indices: US stocks, non-US Stocks, US Bonds, non-US bonds, commodities, and US real estate
+Adjusted_Close_Prices= Historical_Prices['Adj Close'].dropna()                 #Extract Adjusted Close prices from Historial Prices of the six assets 
+returns= srm.logreturns(Returns=Adjusted_Close_Prices)                         #Convert Adjusted Close Prices from daily returns to Logarithmic returns
 
-returns_Figure5= pd.load('returnsMD_Figure5')                                       # Import Daily returns of World Equities, US small-capitalisation premium(Small-Large), growth Premium(Growth-Value) and HFRI Fund of Funds composite Index
+returns_Figure5= pd.load('returnsMD_Figure5')                                  # Import Daily returns of World Equities, US small-capitalisation premium(Small-Large), growth Premium(Growth-Value) and HFRI Fund of Funds composite Index
 
-Table_1_returns= pd.load('Table_1') 
-
-
-
+Table_1_returns= pd.load('Table_1')                                            #Import Table 1 returns for Global Assets, US assets, US sectors, Currencies, US fixed income, US Treasury notes and US credit
 
     #Correlation Surprise
 #Exhibit 5
-Exhibit5_USEquities= pd.load('CorrelationSurprise_Exhibit5_USEquities')
+Exhibit5_USEquities= pd.load('CorrelationSurprise_Exhibit5_USEquities') 
 Exhibit5_EuropeanEquities=pd.load('CorrelationSurprise_Exhibit5_EuropeanEquities')
 Exhibit5_Currency=pd.load('CorrelationSurprise_Exhibit5_Currency')
 
@@ -34,15 +31,15 @@ MSCIUS_PRICES= pd.load('MSCIUSA')                                               
 
 
 
-                            #3: IMPORT SYSTEMIC RISK MAESURES AND RUN MODULES
+                #STAGE 3: IMPORT SYSTEMIC RISK MAESURES AND RUN MODULES
 
     #Mahalanobis Distance
-SRM_mahalanobis= srm.MahalanobisDist(returns)                                       #define Mahalanobis Distance Formula
-Table_1= srm.MahalanobisDist_Table1(Table_1_returns)[0]
-Table_2=srm. MahalanobisDist_Table2(returns, SRM_mahalanobis)
+SRM_mahalanobis= srm.MahalanobisDist(Returns=returns)                                  #Import Mahalanobis Distance Formula given the input of returns
+Table_1= srm.MahalanobisDist_Table1(Returns=Table_1_returns)[0]
+Table_2=srm. MahalanobisDist_Table2(returns, Mahalanobis_Distance_Returns=srm.MahalanobisDist(Returns=returns))#Need to change Returns= Table_2 returns
 
     #Correlation Surprise
-SRM_correlationsurprise= srm. Correlation_Surprise(returns)                        #define Correlation Surprise Score
+SRM_correlationsurprise= srm. Correlation_Surprise(Returns=returns)                        #define Correlation Surprise Score
 Correlation_Surprise_Exhibit_5= srm.Correlation_Surprise_Table_Exhbit5(Exhibit5_USEquities, Exhibit5_EuropeanEquities, Exhibit5_Currency)
 Correlation_Surprise_Exhibit_6= srm.Correlation_Surprise_Table_Exhbit6(SRM_correlationsurprise, Correlation_Surprise_Exhibit_5)
 
